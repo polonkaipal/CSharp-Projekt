74ab3010dd069dfa6cb45a9f0ad613261e2c3b9f
Roxor456 <lpe7702@gmail.com>
2021-06-13 12:58:46 +0200

    PvP & StartWindow -> layout

---
+++ Battleship/StartWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Battleship
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+
+            playerInitialize();
+        }
+
+        private void playerInitialize()
+        {
+            vsComputerRadioBtn.IsChecked = true;
+            player2GridRow.Height = new GridLength(0);
+        }
+
+        private void vsComputerRadioBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            vsPlayerRadioBtn.IsChecked = false;
+            player2GridRow.Height = new GridLength(0);
+        }
+
+        private void vsPlayerRadioBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            vsComputerRadioBtn.IsChecked = false;
+            player2GridRow.Height = new GridLength(1, GridUnitType.Star);
+        }
+
+        private void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
