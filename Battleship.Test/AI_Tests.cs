b9b2125f3475ee21844cbb85cd930dfc8b87d2b6
Polonkai Pál <polonkaipal5@gmail.com>
2021-06-14 23:39:27 +0200

    AI test unit előkészítése

---
+++ Battleship.Test/AI_Tests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Battleship.Test
+{
+    [TestClass]
+    public class AI_Tests
+    {
+        [TestMethod]
+        public void AI_Test()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
