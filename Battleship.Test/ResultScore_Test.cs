1dd02a51016ab9b928e509eb21e88cf75178ea92
Polonkai Pál <polonkaipal5@gmail.com>
2021-06-15 08:13:17 +0200

    Megfelelő framework beállítása a teszthez, újabb teszt osztály

---
+++ Battleship.Test/ResultScore_Test.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Battleship.Test
+{
+    [TestClass]
+    class ResultScore_Test
+    {
+        [TestMethod]
+        public void Score_Test()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
