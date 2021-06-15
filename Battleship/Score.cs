26ad66afb16477bb85ca693cadd1cbc2e08c60d3
Polonkai Pál <polonkaipal5@gmail.com>
2021-06-15 08:51:58 +0200

    Score osztály javítása

--- Battleship/Score.cs
+++ Battleship/Score.cs
@@ -10,5 +10,21 @@ namespace Battleship
         public int PlayerHits { get; set; }
         public int EnemyHits { get; set; }
         public int Rounds { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Score score &&
+                   Player == score.Player &&
+                   Enemy == score.Enemy &&
+                   Winner == score.Winner &&
+                   PlayerHits == score.PlayerHits &&
+                   EnemyHits == score.EnemyHits &&
+                   Rounds == score.Rounds;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Player, Enemy, Winner, PlayerHits, EnemyHits, Rounds);
+        }
     }
 }
