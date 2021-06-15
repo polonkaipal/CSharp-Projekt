08272a47e99902c0863a1ec4f89821b109784ea5
Polonkai Pál <polonkaipal5@gmail.com>
2021-06-15 08:51:09 +0200

    ScoreResult tesztek

--- Battleship.Test/ResultScore_Test.cs
+++ Battleship.Test/ResultScore_Test.cs
@@ -1,18 +1,50 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Battleship.Test
 {
     [TestClass]
-    class ResultScore_Test
+    public class ResultScore_Test
     {
-        [TestMethod]
-        public void Score_Test()
-        {
-            // Arrange
+        readonly private string _filename = "unit_test.json";
+        readonly private string _filename2 = "unit_test2.json";
 
-            // Act
+        [TestMethod]
+        public void ReadResult_ScoreFileDoesntExist_ReturnNull()
+        {
+            // Arrange + Act
+            List<Score> scores = ScoreResult.ReadResult(_filename);
 
             // Assert
+            Assert.IsNull(scores);
+        }
+
+        [TestMethod]
+        public void WriteResult_NewScorePutsScoreFile_ScoreFileContainsNewScore()
+        {
+            // Arrange
+            Score s = new() { 
+                Enemy = "AI",
+                EnemyHits = 3,
+                Player = "My",
+                PlayerHits = 15,
+                Rounds = 5,
+                Winner = "My"
+            };
+
+            List<Score> scores = new List<Score>();
+            scores.Add(s);
+
+            // Act
+            ScoreResult.WriteResult(scores, _filename2);
+
+            // Assert
+            List<Score> tmp = ScoreResult.ReadResult(_filename2);
+            if (!tmp.Contains(s))
+            {
+                Assert.Fail();
+            }
         }
     }
 }
